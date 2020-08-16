076956e96f3d943da9eb4f068fa5a4bfa2352a53
Petr Tripolsky <tripolskypetr@gmail.com>
2020-08-16 16:38:21 +0300

    cleanup

--- packages/form-generator-app/src/pages/one.tsx
+++ packages/form-generator-app/src/pages/one.tsx
@@ -24,6 +24,92 @@ namespace app {
 
   export namespace pages {
 
+    /*const fields: form.IField[] = [
+      {
+        type: FieldType.Expansion,
+        columns: '12',
+        title: 'title',
+        description: 'subtitle',
+        fields: [
+          {
+            type: FieldType.Group,
+            columns: '6',
+            fields: [
+              {
+                type: FieldType.Line,
+                title: 'Колонка слева',
+                columns: '12',
+              },
+              {
+                columns: '12',
+                name: 'string.a',
+                type: FieldType.String,
+                title: 'Some text field',
+                isInvalid: (obj) => obj.string.a.length < 3 ? 'Field len < 3' : null,
+                isDisabled: (obj) => obj.string.b.length > 5,
+                isVisible: (obj) => obj.string.b.length < 10,
+                description: 'Some hint',
+              },
+            ]
+          },
+          {
+            type: FieldType.Group,
+            columns: '6',
+            fields: [
+              {
+                columns: '6',
+                name: 'string.b',
+                type: FieldType.String,
+                title: 'Some text field',
+                description: 'Some hint',
+              },
+              {
+                columns: '6',
+                name: 'string.b',
+                type: FieldType.String,
+                title: 'Some text field',
+                description: 'Some hint',
+                isDisabled: (obj) => !obj.bool.test,
+                outlined: false,
+              },
+              {
+                type: FieldType.Switch,
+                name: 'bool.test',
+                title: 'Switch',
+                isDisabled: (obj) => !obj.bool.anotherTest,
+                columns: '6',
+              },
+              {
+                type: FieldType.Checkbox,
+                name: 'bool.anotherTest',
+                isDisabled: (obj) => !obj.bool.test,
+                title: 'Checkbox',
+                columns: '6',
+              },
+              {
+                type: FieldType.Radio,
+                name: 'radio.field',
+                title: 'Gender 1',
+                radioValue: 'gender1',
+              },
+              {
+                type: FieldType.Radio,
+                name: 'radio.field',
+                title: 'Gender 2',
+                radioValue: 'gender2',
+              },
+              {
+                type: FieldType.Radio,
+                name: 'radio.field',
+                title: 'Gender 3',
+                radioValue: 'gender3',
+              }
+            ]
+          },
+        ]
+      }
+    ];*/
+
     const fields: form.IField[] = [
       {
         type: FieldType.Group,
@@ -175,6 +261,7 @@ namespace app {
       const onSave = useCallback(() => {
         data.patch(changedObj);
         alert('Сохранено!');
+        setChangedObj(null);
       }, [changedObj]);
 
       return (
