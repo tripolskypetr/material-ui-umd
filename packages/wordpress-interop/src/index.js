77f798f067f6e5013919e3612b3c8aa0154cc3f0
Petr Tripolsky <tripolskypetr@gmail.com>
2020-10-18 22:33:12 +0300

    begin-wordpress-interop

---
+++ packages/wordpress-interop/src/index.js
@@ -0,0 +1,81 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+
+const ignoreList = new Set(['index.js', '.DS_Store']);
+
+function printTree(root) {
+  printTree.prototype.space = printTree.prototype.space || 0;
+  var line = [];
+  for (var i = 0; i != printTree.prototype.space; i++) {
+    if (i % 2 == 0) {
+      line.push("|");
+    } else if (printTree.prototype.space - 1 != i) {
+      line.push(" ");
+    } else {
+      line.push("-");
+    }
+  }
+  line.push(root.name);
+  console.log(line.join(""));
+  if (root.child) {
+    printTree.prototype.space += 2;
+    for (var i = 0; i != root.child.length; i++) {
+      printTree(root.child[i]);
+    }
+    printTree.prototype.space -= 2;
+  }
+  else {
+    return;
+  }
+}
+
+function buildTree(folder) {
+  return fs.readdirSync(folder)
+    .filter(f => f != "." && f != "..")
+    .map(f => {
+      const file = path.join(folder, f);
+      if (ignoreList.has(f)) {
+        return null;
+      } else if (fs.lstatSync(file).isFile()) {
+        return { name: f }
+      } else {
+        return {
+          name: f,
+          child: buildTree(file)
+        };
+      }
+    })
+    .filter(v => v !== null);
+}
+
+function buildOne(tree) {
+  const obj = {};
+  obj.title = tree.name;
+  if ('length' in tree) {
+    return tree
+      .map((f) => buildOne(f));
+  } else if ('child' in tree) {
+    obj.type = 'group';
+    obj.fields = tree.child
+      .map((f) => buildOne(f));
+  } else {
+    obj.type = 'string';
+    obj.name = tree.name;
+  }
+  return obj;
+}
+
+function buildFields(folder) {
+  return buildOne(buildTree(folder));
+}
+
+if (require.main === module) {
+  printTree({
+    name: process.cwd(),
+    child: buildTree(process.cwd())
+  })
+} else {
+  module.exports = {buildTree, buildFields};
+}
