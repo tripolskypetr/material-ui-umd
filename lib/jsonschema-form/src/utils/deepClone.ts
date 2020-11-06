5f48536e32b3694c78a597f65f76f2a23f17614c
Petr Tripolsky <tripolskypetr@gmail.com>
2020-11-06 18:56:41 +0300

    array-clone-todo

--- lib/jsonschema-form/src/utils/deepClone.ts
+++ lib/jsonschema-form/src/utils/deepClone.ts
@@ -1,5 +1,9 @@
 namespace form {
 
+  const {
+    isArray,
+  } = Array;
+
   export namespace utils {
 
     const isObject = (obj) => {
@@ -11,8 +15,10 @@ namespace form {
       const target = {};
       for (const prop in src) {
         if (src.hasOwnProperty(prop)) {
-          // if the value is a nested object, recursively copy all it's properties
-          if (isObject(src[prop])) {
+          if (isArray(src[prop])) {
+            /* TODO: нет поддержки копирования массивов объектов */
+            target[prop] = src[prop].slice(0);
+          } else if (isObject(src[prop])) {
             target[prop] = deepClone(src[prop]);
           } else {
             target[prop] = src[prop];
