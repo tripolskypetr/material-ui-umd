e04d27c0ec2fc6ed2562aab78358e0d6bc94f99e
Petr Tripolsky <tripolskypetr@gmail.com>
2020-11-06 20:17:06 +0300

    pickers-typedef

---
+++ src/pickers/index.ts
@@ -0,0 +1,23 @@
+declare namespace material {
+  export namespace pickers {
+    export function useUtils(...args: any): any;
+    export function makePickerWithState(...args: any): any;
+    export function useKeyboardPickerState(...args: any): any;
+    export function usePickerState(...args: any): any;
+    export function useStaticState(...args: any): any;
+    export function validate(...args: any): any;
+    export const Calendar: Component;
+    export const DatePicker: Component;
+    export const KeyboardDatePicker: Component;
+    export const Picker: Component;
+    export const Day: Component;
+    export const Clock: Component;
+    export const ClockView: Component;
+    export const TimePickerView: Component;
+    export const KeyboardTimePicker: Component;
+    export const TimePicker: Component;
+    export const DateTimePicker: Component;
+    export const KeyboardDateTimePicker: Component;
+    export const MuiPickersUtilsProvider: Component;
+  }
+}
