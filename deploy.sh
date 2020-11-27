d6470169a893acd9beb1378809c408aadc92b51e
Petr Tripolsky <tripolskypetr@gmail.com>
2020-11-28 00:41:10 +0300

    single deploy script

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+rm dist/*
+
+npm install
+npm run build:umd
+npm run build:types
+npm run build:lib
+
+cp ./lib/**/dist/*.js ./dist
+cp ./lib/**/dist/*.d.ts ./dist
+
+for D in `find ./dist -type f -not -name "*.min.js" -name "*.js"`
+do
+npx google-closure-compiler --js $D --js_output_file $D.tmp
+rm $D
+mv $D.tmp $D
+done
--- lib/build.sh
+++ lib/build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 for D in `find . -maxdepth 1 -not -path "." -not -path "./.*" -type d`
 do
     cd $D
