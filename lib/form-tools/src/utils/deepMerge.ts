2c2119352be014db5b9a6425a753101095b20d6d
Petr Tripolsky <tripolskypetr@gmail.com>
2021-01-15 12:34:47 +0300

    deepMerge-array-support

--- lib/form-tools/src/utils/deepMerge.ts
+++ lib/form-tools/src/utils/deepMerge.ts
@@ -2,14 +2,16 @@ namespace form {
 
   export namespace utils {
 
-    const isObject = (item) => (item && typeof item === 'object' && !Array.isArray(item));
+    const isObject = (item) => item && typeof item === "object" && !Array.isArray(item);
 
     export const deepMerge = (target, ...sources) => {
       if (!sources.length) return target;
       const source = sources.shift();
       if (isObject(target) && isObject(source)) {
         for (const key in source) {
-          if (isObject(source[key])) {
+          if (Array.isArray(source[key])) {
+            target[key] = source[key].slice(0);
+          } else if (isObject(source[key])) {
             if (!target[key]) Object.assign(target, { [key]: {} });
             deepMerge(target[key], source[key]);
           } else {
